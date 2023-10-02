33896996bb3712c678b5533c1c8be85a1b9ca537
Carole Gérard <gerard.carole78@gmail.com>
2023-10-02 11:01:05 +0200

    exo_1 : affichage dans navigateur

--- script/exo_1.js
+++ script/exo_1.js
@@ -31,3 +31,21 @@ console.log('Reykjavik : ', reykjavik);
 // Saint-Petersburg (Russia)
 let saintPetersburg = dateLocale.toLocaleString('fr-EU',{timeZone: 'Europe/Moscow'});
 console.log('Saint-Petersburg : ', saintPetersburg);
+
+
+
+let exo1 = document.querySelector('.exo1');
+
+let brusselsParag = document.createElement('p');
+brusselsParag.textContent = 'Bruxelles : ' + brussels;
+exo1.appendChild(brusselsParag);
+let anchorageParag = document.createElement('p');
+anchorageParag.textContent = 'Anchorage : ' + anchorage;
+exo1.appendChild(anchorageParag);
+let reykjavikParag = document.createElement('p');
+reykjavikParag.textContent = 'Reykjavik : ' + reykjavik;
+exo1.appendChild(reykjavikParag);
+let saintPetersburgParag = document.createElement('p');
+saintPetersburgParag.textContent = 'Saint-Petersburg : ' + saintPetersburg;
+exo1.appendChild(saintPetersburgParag);
+
